93a958b40705d931476307990933473967539d52
Joshuawadd <joshua.j.waddington@durham.ac.uk>
2019-12-05 04:53:34 +0000

    mostly completed server annd client files, just need to work on error handling and logging.

--- client.py
+++ client.py
@@ -3,33 +3,99 @@
 import sys
 from socket import *
 import pickle
+
+
+def getBoards():
+    instruction = ["GET_BOARDS"]
+    clientSocket.send(pickle.dumps(instruction))
+    length = pickle.loads(clientSocket.recv(10))
+    pickleBoardList = clientSocket.recv(length)
+    boardList = pickle.loads(pickleBoardList)
+    a = 1
+    print("From server: ")
+    for i in boardList:
+        print(a, ': ', i)
+        a = a + 1
+    return boardList
+
+
+def post():
+    information = []
+    instruction = "POST"
+    information.append(instruction)
+    numSelection = True
+    while numSelection:
+        boardNum = input('Input the board number: ')
+        try:
+            if len(boardList) >= int(boardNum) >= 1:
+                numSelection = False
+                print("Not a valid board selection.")
+            else:
+                numSelection = True
+                print("Not a valid board selection.")
+        except:
+            numSelection = True
+            print("Please enter a number for the board selection.")
+    information.append(boardNum)
+    title = input('Input the title: ')
+    information.append(title)
+    message = input('Input the message: ')
+    information.append(message)
+    clientSocket.send(pickle.dumps(information))
+    length = pickle.loads(clientSocket.recv(10))
+    print(pickle.loads(clientSocket.recv(length)))
+
+
+def getMessages(boardNum):
+    information = ["GET_MESSAGES", boardNum]
+    clientSocket.send(pickle.dumps(information))
+    length = pickle.loads(clientSocket.recv(10))
+    print(length)
+    messageList = pickle.loads(clientSocket.recv(length))
+    if messageList == []:
+        print("There are no messages on this board")
+    else:
+        print("Most recent messages from this board: ")
+        print()
+        for i in reversed(messageList):
+            print(i[0] , " : ", '"', i[1], '"')
+    print()
+
+def quit():
+    clientSocket.close()
+    sys.exit()
+
+
 serverName = sys.argv[1]
 serverPort = int(sys.argv[2])
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-boardlist = clientSocket.recv(1024)
-a=1
-for i in pickle.loads(boardlist):
-    print('From Server: ', a, ': ', i)
-    a=a+1
-clientSocket.close()
-command = input('Input a command:')
-if command == "POST":
-    POST()
-elif command == "QUIT":
-    QUIT()
+length = pickle.loads(clientSocket.recv(10))
+pickleBoardList = clientSocket.recv(length)
+boardList = pickle.loads(pickleBoardList)
+print(boardList)
+a = 1
+print("From server: ")
+for i in boardList:
+    print(a, ': ', i)
+    a = a + 1
+print("Please enter one of the following commands:")
+print()
+print("POST - to post a message to one of the boards,")
+print("GET_BOARDS - to view again the list of available boards,")
+print("Or the number of the board you would like to see the most recent messages from.")
+print()
+while True:
+    command = input('Input a command:')
+    if command == "POST":
+        post()
+    elif command == "GET_BOARDS":
+        getBoards()
+    elif command == "QUIT":
+        quit()
+    elif command.isdigit():
+        if len(boardList) >= int(command) >= 1:
+            getMessages(command)
 
 
-def GET_BOARDS() :
-    boards = os.listdir(path='board')
-    print(boards[1])
 
-
-def POST() :
-    boardnum = input('Input the board number: ')
-    title = input('Input the title: ')
-    message = input('Input the message: ')
-
-
-def QUIT(a) :
-    clientSocket.close()
--- server.py
+++ server.py
@@ -1,35 +1,129 @@
 #!/usr/bin/env python3
 
-import os
+
 from socket import *
-from pathlib import Path
+import select
+import sys
+import os
 import pickle
-boards = os.listdir(path='board')
-#print(boards)
-p = Path('./board')
-print(p)
-#print([x for x in p.iterdir() if x.is_dir()])
-#print(os.fspath(f))
-#print(p.resolve())
-serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-print('The server is ready to receive')
-while True:
-    connectionSocket, addr = serverSocket.accept()
-    boards1 = pickle.dumps(boards)
-    print(boards1)
-    connectionSocket.send(boards1)
-    connectionSocket.close()
+from datetime import datetime
 
-def GET_BOARDS() :
+
+def messageLen(data, client):
+    length = len(pickle.dumps(data))
+    client.send(pickle.dumps(length))
+
+
+def getBoards(client):
+    os.chdir(cwd)
     boards = os.listdir(path='board')
-    print(boards[1])
+    print("sending")
+    messageLen(boards, client)
+    client.send(pickle.dumps(boards))
 
-def POST() :
-    boardnum = input('Input the board number: ')
-    title = input('Input the title: ')
-    message = input('Input the message: ')
 
-#def QUIT() :
+def post(boardNum, title, message, client) :
+    os.chdir(cwd)
+    boards = os.listdir(path='board')
+    title = title.replace(' ', '_')
+    now = datetime.now()
+    time = now.strftime("%Y%m%d-%H%M%S")
+    board = boards[int(boardNum)-1]
+    boardDir = cwd + '/board'
+    chosenBoardDir = boardDir + "/" + board
+    os.chdir(chosenBoardDir)
+    messageFile = open(time + '-' + title, "w+")
+    messageFile.write(message)
+    messageFile.close()
+    success = "Message posted"
+    messageLen(success, client)
+    client.send(pickle.dumps(success))
+    print("Successful")
+
+
+def getMessages(boardNum,  client):
+    os.chdir(cwd)
+    boards = os.listdir(path='board')
+    messages = []
+    board = boards[int(boardNum) - 1]
+    boardDir = cwd + '/board'
+    chosenBoardDir = boardDir + "/" + board
+    os.chdir(chosenBoardDir)
+    for file in os.listdir(chosenBoardDir):
+        f = open(file, "r")
+        if f.mode == "r":
+            contents = f.read()
+        messages.append([file, contents])
+    messages = messages[-100:]
+    messageLen(messages, client)
+    client.send(pickle.dumps(messages))
+    print("Successful")
+
+
+
+serverName = sys.argv[1]
+serverPort = int(sys.argv[2])
+serverSocket = socket(AF_INET,SOCK_STREAM)
+try:
+    serverSocket.bind((serverName, serverPort))
+    print(f'The server is ready to recieve on {serverName}:{serverPort}...')
+
+except IOError as error:
+    print('The server port is busy or doesn\'t exist.'.format(str(error)))
+    sys.exit()
+serverSocket.listen(1)
+
+socketsList = [serverSocket]
+clients = {}
+
+cwd = os.getcwd()
+
+serverSocket.setblocking(False)
+
+while True:
+
+    readSockets, _, exceptionSockets = select.select(socketsList, [], socketsList)
+
+    # Iterate over notified sockets
+    for currentSocket in readSockets:
+
+        # If notified socket is a server socket - new connection, accept it
+        if currentSocket == serverSocket:
+            # Accept new connection
+            # That gives us new socket - client socket, connected to this given client only, it's unique for that client
+            clientSocket, clientAddress = serverSocket.accept()
+
+            socketsList.append(clientSocket)
+
+            print('Accepted new connection from {}:{}'.format(*clientAddress))
+            getBoards(clientSocket)
+        else:
+            try:
+                pickleData = currentSocket.recv(1024)
+            except:
+                # Remove from list for socket.socket()
+                socketsList.remove(currentSocket)
+                print('Disconnection from {}:{}'.format(*clientAddress))
+
+                # Remove from our list of users
+
+                pickleData = []
+
+                continue
+            try:
+                data = pickle.loads(pickleData)
+            except EOFError:
+                data = []
+            if data:
+                instruction = data[0]
+                if instruction == "GET_BOARDS":
+                    getBoards(clientSocket)
+                elif instruction == "POST":
+                    boardNum = data[1]
+                    title = data[2]
+                    message = data[3]
+                    post(boardNum, title, message, clientSocket)
+                elif instruction == "GET_MESSAGES":
+                    getMessages(data[1], clientSocket)
+
+
